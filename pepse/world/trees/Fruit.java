10fcc7f9d41b98080cdd2383fbd2a7fd7748c2d3
Ohad leshno <ohadleshno@gmail.com>
2024-06-19 14:14:58 +0200

    change color of fruit

--- pepse/world/trees/Fruit.java
+++ pepse/world/trees/Fruit.java
@@ -4,6 +4,7 @@ import danogl.GameObject;
 import danogl.collisions.Collision;
 import danogl.collisions.GameObjectCollection;
 import danogl.components.GameObjectPhysics;
+import danogl.gui.rendering.OvalRenderable;
 import danogl.gui.rendering.RectangleRenderable;
 import danogl.util.Vector2;
 import pepse.world.Avatar;
@@ -15,16 +16,16 @@ import static pepse.world.trees.Flower.FLOWER_HEIGHT;
 import static pepse.world.trees.Flower.FLOWER_WIDTH;
 
 public class Fruit extends GameObject {
-    public static final int FRUIT_WIDTH = FLOWER_WIDTH / 3 * 2;
-    public static final int FRUIT_HEIGHT = FLOWER_HEIGHT / 3 * 2;
-    public static final Color FRUIT_COLOR = new Color(189, 19, 84);
+    public static final int FRUIT_WIDTH = FLOWER_WIDTH / 4 * 3;
+    public static final int FRUIT_HEIGHT = FLOWER_HEIGHT / 4 * 3;
+    public static final Color FRUIT_COLOR = new Color(107, 19, 189);
     public static final Vector2 FRUIT_SIZE = Vector2.ONES.multY(FRUIT_HEIGHT).multX(FRUIT_WIDTH);
     private Consumer<Float> addEnergy;
     private GameObjectCollection gameObjects;
     private boolean isEaten = false;
 
     public Fruit(Vector2 fruitTopLeftRight, Consumer<Float> addEnergy, GameObjectCollection gameObjects) {
-        super(fruitTopLeftRight.subtract(Vector2.DOWN.mult(FRUIT_HEIGHT)), FRUIT_SIZE, new RectangleRenderable(FRUIT_COLOR));
+        super(fruitTopLeftRight.subtract(Vector2.DOWN.mult(FRUIT_HEIGHT)), FRUIT_SIZE, new OvalRenderable(FRUIT_COLOR));
         this.physics().setMass(GameObjectPhysics.IMMOVABLE_MASS);
         this.addEnergy = addEnergy;
         this.gameObjects = gameObjects;
